c655998035056d24be04d3e68fe47fe8556d8d14
Damian Rosinski <d.rosinski256@gmail.com>
2018-11-01 22:22:22 +0100

    Dodanie klasy ChekersData

---
+++ src/checkers/ChekersData.java
@@ -0,0 +1,19 @@
+package checkers;
+
+public class ChekersData {
+    // Klasa ktora przechowuje informacje o Białych i Czarnych pionkach.
+    // Informacje o polozeniu pionkow na mapie itp. rozwinie się jeszcze
+
+    // Białe Pionki to 1 a czarne to 2 i to będzie na tablicy intow
+    public static final int EMPTY = 0, WHITE = 1, BLACK = 2;
+
+
+    private int board [][];     // Mapa intow - tak naprawde to bedzie nasza plansza pomocnicza - board[r][c]
+
+    public ChekersData(){       //Konstruktor
+        board = new int[8][8];  // Nasza plansza
+        setUpGame();            //Funkcja ktora ustawi pionki na planszy
+    }
+
+    public void setUpGame(){} // Trzeba odpowiednio ustawic pionki na planszy
+}
