f9f366ff5ce7d1c755dac2d4809823dab148ba03
Damian Rosinski <d.rosinski256@gmail.com>
2018-12-02 15:54:43 +0100

    Add pawns moving

--- src/checkers_swing/CheckersBoard.java
+++ src/checkers_swing/CheckersBoard.java
@@ -1,10 +1,12 @@
 package checkers_swing;
-
+import data.CheckersMove;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import data.CheckersData;
 
@@ -17,13 +19,18 @@ public class CheckersBoard extends JPanel{
     private ImageIcon image_ico_pawn_black;
     private Image image_pawn_white;
     private Image image_pawn_black;
-    private Point temp_point;
-    ArrayList<Pawn> pawns = new ArrayList<Pawn>();
-    private int[][] board;
+    private Pawn temp_pawn =new Pawn();
+    private Pawn pressed_pawn=null;
+    private Pawn printed_pawn=null;
+    private Pawn dragged_pawn=null;
+
+    HashMap<Point, Pawn> pawns = new HashMap<>();
+    private CheckersData board;
+    private int turn;
     int xx=0, yy=0;
 
     public CheckersBoard() {
-        board = checkersData.getBoard();
+        board = new CheckersData();
 
         image = new ImageIcon(getClass().getResource("/pictures/board.png")).getImage()
                 .getScaledInstance(1024, 768, Image.SCALE_SMOOTH);
@@ -37,49 +44,98 @@ public class CheckersBoard extends JPanel{
                 .getScaledInstance(128,96,Image.SCALE_SMOOTH);
         image_ico_pawn_black = new ImageIcon(image_pawn_black);
 
-        addPawns();
+        addPawns(board.getBoard());
         setPreferredSize(new Dimension(image.getWidth(null), image.getHeight(null)));
 
         //***Listeners****
         addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                int x,y;
-                x=evt.getX();
-                y=evt.getY();
-                xx=x;
-                yy=y;
-                if (pawns.contains(new Pawn(new Point(x/128,y/96),image_ico_pawn_white))
-                        || pawns.contains(new Pawn(new Point(x/128,y/96), image_ico_pawn_black))
-                   )
+                if (pawns.containsKey(new Point(evt.getX()/128,evt.getY()/96)))
                     System.out.println("JAKIS JESSSt!");
-                System.out.println("mouseClicked - X: "+ x/128 +", Y: "+y/96);
             }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
 
+                if (pawns.containsKey(new Point((evt.getX()/128),evt.getY()/96))) {
+                    pressed_pawn = pawns.get(new Point((evt.getX()/128), evt.getY()/96));
+                    System.out.println("Wzialem pionek");
+                    printed_pawn = new Pawn(new Point(evt.getX(),evt.getY()),image_ico_pawn_white);
+                }
+            }
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                if(pressed_pawn!=null) {
+                    dragged_pawn = new Pawn(new Point((evt.getX()/128),evt.getY()/96), pressed_pawn.getImage());
+
+                    int selectedRowFrom = pressed_pawn.point.y;
+                    int selectedColFrom = pressed_pawn.point.x;
+                    int selectedRowTo = dragged_pawn.point.y;
+                    int selectedColTo = dragged_pawn.point.x;
+                    System.out.println("CF"+selectedColFrom+" RF"+selectedRowFrom+" CT"+selectedColTo+" RT"+selectedRowTo);
+
+                    int currentChecker = board.getBoard()[selectedRowFrom][selectedColFrom];
+                    CheckersMove myMove = new CheckersMove(selectedRowFrom, selectedColFrom, selectedRowTo, selectedColTo);
+
+                    if ((selectedRowFrom + 1 == selectedRowTo || selectedRowFrom - 1 == selectedRowTo) && (currentChecker == CheckersData.WHITE || currentChecker == CheckersData.BLACK)) {
+                        if (board.canMove(currentChecker, selectedRowFrom, selectedColFrom, selectedRowTo, selectedColTo, turn)) {
+                            board.makeMove(myMove);
+                            pawns.remove(pressed_pawn.point);//usuwam stary
+                            pawns.put(dragged_pawn.point,dragged_pawn);//wkladamy nowy
+                            changeTurn();
+                        }
+                    } else if ((selectedRowFrom + 2 == selectedRowTo || selectedRowFrom - 2 == selectedRowTo) && (currentChecker == 1 || currentChecker == 2)) {
+                        if (board.canJump(currentChecker, selectedRowFrom, selectedColFrom, selectedRowTo, selectedColTo, turn)) {
+                            board.makeMove(myMove);
+                            int jumpRow = (selectedRowFrom + selectedRowTo) / 2;
+                            int jumpCol = (selectedColFrom + selectedColTo) / 2;
+                            pawns.remove(new Point(jumpCol,jumpRow));
+
+                            pawns.remove(pressed_pawn.point);//usuwam stary
+                            pawns.put(dragged_pawn.point,dragged_pawn);//wkladamy nowy
+                            changeTurn();
+
+                        }
+                    } else if ((currentChecker == CheckersData.WHITE_KING || currentChecker == CheckersData.BLACK_KING)
+                            && board.canKingMoveJump(currentChecker, selectedRowFrom, selectedRowTo, selectedColFrom, selectedColTo, turn)) {
+                        board.kingMove(myMove);
+                        pawns.remove(pressed_pawn.point);//usuwam stary
+                        pawns.put(dragged_pawn.point,dragged_pawn);//wkladamy nowy
+                        changeTurn();
+                    }
+                    System.out.println("From: "+pressed_pawn.toString()+" To: "+ dragged_pawn.toString());
+                }
+
+                    System.out.println("Mouse Release");
+                    repaint();
+                    pressed_pawn = null;
+                    printed_pawn = null;
+
+            }
             public void mouseEntered(java.awt.event.MouseEvent evt) { }
-            public void mousePressed(java.awt.event.MouseEvent evt) { }
             public void mouseExited(java.awt.event.MouseEvent evt) { }
-            public void mouseReleased(java.awt.event.MouseEvent evt) { }
         });
-        addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
-            public void mouseMoved(MouseEvent e) {
-                System.out.println("Mouse moved X: "+e.getX()+" Y: "+e.getY());
-            }
 
+        addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseMoved(MouseEvent e) { }
             public void mouseDragged(MouseEvent e) {
-                System.out.println("Mouse DRAGGED X: "+e.getX()+" Y: "+e.getY());
+
+                if(pressed_pawn!=null) {
+                    //pawns.put(pressed_pawn.point, new Pawn(new Point(e.getX() / 128, e.getY() / 96), pressed_pawn.getImage()));
+                    printed_pawn.setP(e.getX() - 60, e.getY() - 60);
+                    printed_pawn.setImage(pressed_pawn.getImage());
+                    repaint();
+                }
 
             }
         });
         //*****Listeners_END****
     }
 
-    private void addPawns() {
+    private void addPawns(int[][] board) {
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 if (board[row][col] == 1) { //white
-                    pawns.add(new Pawn(new Point(col , row), image_ico_pawn_white));
+                    pawns.put(new Point(col , row), new Pawn(new Point(col, row), image_ico_pawn_white));
                 } else if (board[row][col] == 2) {
-                    pawns.add(new Pawn(new Point(col, row), image_ico_pawn_black));
+                    pawns.put(new Point(col , row), new Pawn(new Point(col, row), image_ico_pawn_black));
                 } else if (board[row][col] == 3)
                     System.out.println("TU POWINIEN BYC KING");
                 else if (board[row][col] == 4)
@@ -93,11 +149,21 @@ public class CheckersBoard extends JPanel{
         super.paintComponent(g);
         image_ico.paintIcon(this, g, 0, 0);
         drawPawns(g);
+        if(printed_pawn!=null)
+        printed_pawn.image.paintIcon(this,g,(int)printed_pawn.point.getX(),(int)printed_pawn.point.getY());
     }
 
     public void drawPawns(Graphics g) {
-        for (Pawn p : pawns) {
-            p.image.paintIcon(this, g, (int) p.point.getX()*128, (int) p.point.getY()*96);
+        for (Map.Entry<Point, Pawn> e : pawns.entrySet()) {
+            e.getValue().image.paintIcon(this, g, (int) e.getValue().point.getX()*128, (int) e.getValue().point.getY()*96);
+        }
+    }
+
+    private void changeTurn() {
+        if (turn == 0) {
+            turn = 1;
+        } else if (turn == 1) {
+            turn = 0;
         }
     }
 }
--- src/checkers_swing/Pawn.java
+++ src/checkers_swing/Pawn.java
@@ -14,9 +14,39 @@ public class Pawn {
         this.image = image;
         posindex=0;
     }
+    public Pawn() {
+    }
+    @Override
+    public String toString() {
+        return "Pawn{" +
+                "point=" + this.point.x +", "+ this.point.y +
+        '}';
+    }
 
-    public void setP(Point p) {
-        this.point = p;
+    public Point getPoint() {
+        return point;
+    }
+
+    public ImageIcon getImage() {
+        return image;
+    }
+
+    public void setImage(ImageIcon image) {
+        this.image = image;
+    }
+
+    public void setP(int x, int y) {
+        this.point.x = x;
+        this.point.y = y;
+    }
+
+
+    public boolean equal(Pawn p) {
+        boolean isEqual=false;
+        if(p!=null && p instanceof Pawn) {
+            isEqual=(this.point==p.point);
+        }
+        return isEqual;
     }
 
 }
